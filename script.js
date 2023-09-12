0d5a8aaf6c70b1b6face853deacb45bffa3fd575
Cilps <125075544+Cilps@users.noreply.github.com>
2023-09-12 13:17:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function hide_marquee_h3() {
+    document.getElementById("marquee_h3").style.opacity = "0";
+}
+function show_marquee_h3() {
+    document.getElementById("marquee_h3").style.opacity = "1";
+}
